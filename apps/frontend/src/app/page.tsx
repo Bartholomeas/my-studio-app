b64e8cadcf327c158ff259f2d950524866e04330
Bartosz <barth.webdesign@gmail.com>
2024-09-24 20:30:13 +0200

    feat: init shadcn

---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <main className="relative">
+      <main>
+        <Button>Kliku</Button>
+      </main>
+    </main>
+  );
+};
+
+export default Home;
