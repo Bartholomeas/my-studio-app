a97fcc068fd6f39d25eced1bb38736145639b4f2
Bartholomeas <69473527+Bartholomeas@users.noreply.github.com>
2024-09-25 15:48:12 +0200

    feat: add scrollable gallery section (#2)
    Co-authored-by: Bartosz <barth.webdesign@gmail.com>

--- apps/frontend/src/components/layout/footer.tsx
+++ apps/frontend/src/components/layout/footer.tsx
@@ -4,8 +4,8 @@ export const Footer = () => {
   return (
     <footer className="relative h-[800px]"
       style={{ clipPath: 'polygon(0% 0, 100% 0%, 100% 100%, 0 100%)' }}>
-      <div className="relative container h-[calc(100vh+800px)] -top-[100vh] w-full bottom-0 bg-[#ff991c]">
-        <div className="sticky top-[calc(100vh-800px)] h-[800px] ">
+      <div className="relative h-[calc(100vh+800px)] -top-[100vh] w-full bottom-0 bg-[#ff991c]">
+        <div className="sticky container top-[calc(100vh-800px)] h-[800px] ">
           <div className="flex gap-4 pt-[calc(100vh-800px)]">
             <div className="flex flex-col gap-2">
               <h3>Titl</h3>
