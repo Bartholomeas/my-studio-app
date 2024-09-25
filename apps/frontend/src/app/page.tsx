e0b5ce4e98a1fcbab4c5159a13f1cd8ad201b68e
Bartosz <barth.webdesign@gmail.com>
2024-09-25 11:48:20 +0200

    chore: remove eslint default export rule

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -3,13 +3,13 @@ import { LandingSection } from "@/components/home/sections/landing-section";
 import { HomeMainWrapper } from "@/components/home/wrappers/home-main-wrapper";
 
 const Home = () => {
-	return (
-		<HomeMainWrapper className="relative h-[200vh]">
-			<LandingSection />
-			<AboutSection />
-			<div className="h-screen bg-white" />
-		</HomeMainWrapper>
-	);
+  return (
+    <HomeMainWrapper className="relative h-[200vh]">
+      <LandingSection />
+      <AboutSection />
+      <div className="h-screen bg-white" />
+    </HomeMainWrapper>
+  );
 };
 
 export default Home;
