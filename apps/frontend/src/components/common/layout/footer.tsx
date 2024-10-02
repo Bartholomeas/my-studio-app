b80e8e5def23066868caf9c6fcc88411141b67c6
Bartosz <barth.webdesign@gmail.com>
2024-10-02 09:51:27 +0200

    fix: add variable color to footer

--- apps/frontend/src/components/common/layout/footer.tsx
+++ apps/frontend/src/components/common/layout/footer.tsx
@@ -11,7 +11,7 @@ export const Footer = () => {
       className="relative h-[600px]"
       style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
     >
-      <div className="relative bottom-0 top-[-100vh] h-[calc(100vh+600px)] w-full bg-[#ff991c]">
+      <div className="relative bottom-0 top-[-100vh] h-[calc(100vh+600px)] w-full bg-primary">
         <div className="container sticky top-[calc(100vh-600px)] flex h-[600px] flex-col justify-end pb-4">
           <div className="flex flex-wrap gap-8 pt-8">
             <div className="flex flex-col gap-4">
