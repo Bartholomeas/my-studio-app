f4b87dec0bd11cc3d31f0c8b839f137a530747c7
Bartholomeas <69473527+Bartholomeas@users.noreply.github.com>
2024-10-06 13:14:58 +0200

    feat: hero masked hover section (#13)
    * feat: init hero masked hover section
    
    * feat: add hovering mask
    
    ---------
    
    Co-authored-by: Bartosz <barth.webdesign@gmail.com>

---
+++ apps/frontend/src/components/common/special/cursor-handler/use-mouse-position-context.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import {createContext, type PropsWithChildren, useContext} from "react";
+
+import {useMousePosition} from "./use-mouse-position";
+
+type MousePositionContextProps = ReturnType<typeof useMousePosition>;
+
+const MousePositionContext = createContext<MousePositionContextProps | undefined>(undefined);
+
+export const MousePositionProvider = ({children}: PropsWithChildren) => {
+    const mousePosition = useMousePosition();
+    return <MousePositionContext.Provider value={mousePosition}>{children}</MousePositionContext.Provider>;
+};
+
+export const useMousePositionContext = () => {
+    const context = useContext(MousePositionContext);
+
+    if (!context) {
+        throw new Error("useMousePositionContext must be used within a MousePositionProvider");
+    }
+
+    return context;
+};
