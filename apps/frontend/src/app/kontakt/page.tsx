7338b800202bb5ef4bfe0f32b84cda69dc05fdf7
Bartholomeas <69473527+Bartholomeas@users.noreply.github.com>
2024-10-15 15:03:49 +0200

    chore: minor structure of project improvemnts (#18)
    Co-authored-by: Bartosz <barth.webdesign@gmail.com>

--- apps/frontend/src/app/kontakt/page.tsx
+++ apps/frontend/src/app/kontakt/page.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 import { APP_ROUTES } from "@/misc/routes";
 
+
 const ContactPage = () => {
-	return (
-		<main className={"flex h-screen items-center justify-center bg-white"}>
-			<p>Xdd</p>
-			<Link href={APP_ROUTES.home.href}>{APP_ROUTES.home.label}</Link>
-		</main>
-	);
+  return (
+    <main className={"flex h-screen items-center justify-center bg-white"}>
+      <p>Xdd</p>
+      <Link href={APP_ROUTES.home.href}>{APP_ROUTES.home.label}</Link>
+    </main>
+  );
 };
 
 export default ContactPage;
