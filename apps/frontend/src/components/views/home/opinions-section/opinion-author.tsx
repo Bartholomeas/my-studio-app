1e8f9fbb278e834f8995036553e7ea98bee0967b
Bartosz <bstefaniak@estrategysolutions.com>
2025-03-31 22:58:33 +0200

    chore: opinions improvements

--- apps/frontend/src/components/views/home/opinions-section/opinion-author.tsx
+++ apps/frontend/src/components/views/home/opinions-section/opinion-author.tsx
@@ -1,36 +1,35 @@
 import Image from "next/image";
-import {Text} from "@/components/common/text";
+
+import { Text } from "@/components/common/text";
 
 export interface OpinionAuthorProps {
-    image: string,
-    authorName: string,
-    role?: string,
-    company?: string
+  image: string,
+  authorName: string,
+  role?: string,
+  company?: string;
 
 }
 
-export const OpinionAuthor = ({image, authorName, role, company}: OpinionAuthorProps) => {
-    return (
-        <div className={"flex items-center gap-4 mt-4"}>
-            <Image
-                src={image}
-                loading={"lazy"}
-                // placeholder={"blur"}
-                alt={"Team member"}
-                height={60}
-                width={60}
-                // fill
-                className={"rounded-full aspect-square object-cover"}
-            />
+export const OpinionAuthor = ({ image, authorName, role, company }: OpinionAuthorProps) => {
+  return (
+    <div className={"mt-4 flex items-center gap-4"}>
+      <Image
+        src={image}
+        loading={"lazy"}
+        alt={"Team member"}
+        height={60}
+        width={60}
+        className={"aspect-square rounded-full object-cover"}
+      />
 
-            <div className={'flex flex-col'}>
-                <Text weight={'semibold'} size={'md'}>
-                    {authorName}
-                </Text>
-                <Text size={'md'}>
-                    {role && <span>{role}{company ? `, ${company}` : ''}</span>}
-                </Text>
-            </div>
-        </div>
-    )
-}
+      <div className={'flex flex-col'}>
+        <Text weight={'semibold'} size={'lg'}>
+          {authorName}
+        </Text>
+        <Text size={'md'}>
+          {role && <span>{role}{company ? `, ${company}` : ''}</span>}
+        </Text>
+      </div>
+    </div>
+  );
+};
