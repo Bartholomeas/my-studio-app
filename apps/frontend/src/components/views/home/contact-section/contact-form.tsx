d9c4f970d028cf2a09d3ea01b5718cc0d2279006
Bartosz <bstefaniak@estrategysolutions.com>
2025-04-25 00:01:23 +0200

    chore: minor adjustments

--- apps/frontend/src/components/views/home/contact-section/contact-form.tsx
+++ apps/frontend/src/components/views/home/contact-section/contact-form.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 
+import { useRef } from "react";
+
 import { zodResolver } from "@hookform/resolvers/zod";
+import { motion, useInView } from "framer-motion";
 import { useTranslations } from "next-intl";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -25,91 +28,106 @@ const contactFormSchema = z.object({
 
 export const ContactForm = () => {
   const t = useTranslations("/.contactSection");
-
   const form = useForm<z.infer<typeof contactFormSchema>>({
     resolver: zodResolver(contactFormSchema),
   });
 
-
+  const contactFormRef = useRef<HTMLDivElement>(null);
+  const isContactFormInView = useInView(contactFormRef, {
+    once: true,
+    amount: 0.8,
+  });
 
   const onSubmit = (data: z.infer<typeof contactFormSchema>) => {
     console.log(data);
   };
 
-
   return (
-    <Card variant={'primary'} className={"col-span-12 flex h-full flex-col md:col-span-8"}>
-      <div className={"flex flex-col gap-2 px-6 py-4"}>
+    <motion.div
+      ref={contactFormRef}
+      initial={{ opacity: 0, y: 250 }}
+      animate={isContactFormInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 250 }}
+      transition={{
+        type: "spring",
+        stiffness: 400,
+        damping: 15,
+        duration: 1.6
 
-        <Title type={"h2"} size={"h1"} weight={"bold"} >
-          {t("title.base")}
-          <br />
-          <Text as={"span"} size={"h1"} weight={"bold"} >{t("title.highlighted")}</Text>
-        </Title>
-        <Text size={"lg"}  >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus eum dolor sunt, libero modi tempore.</Text>
-      </div>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className={"flex flex-col gap-4 p-8"}>
-          <FormField
-            control={form.control}
-            name={"email"}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className={"font-bold"}>Email</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name={"name"}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className={"font-bold"}>Imię</FormLabel>
-                <FormControl>
-                  <Input  {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name={"title"}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className={"font-bold"}>Tytuł</FormLabel>
-                <FormControl>
-                  <Input  {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name={"message"}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel className={"font-bold"}>Wiadomość</FormLabel>
-                <FormControl>
-                  <Textarea {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button
-            data-hover={CursorActionType.SEND}
-            data-hover-size={100}
-            size={'lg'}
-            className={"mt-4 w-full self-end bg-foreground !text-background"}
-            type={"submit"}>Wyślij wiadomość</Button>
-        </form>
-      </Form>
-    </Card>
+      }}
+      className={"col-span-12 flex h-full flex-col md:col-span-8"}>
+      <Card variant={'primary'} className={"size-full"}>
+        <div className={"flex flex-col gap-2 px-6 py-4"}>
+
+          <Title type={"h2"} size={"h1"} weight={"bold"} >
+            {t("title.base")}
+            <br />
+            <Text as={"span"} size={"h1"} weight={"bold"} >{t("title.highlighted")}</Text>
+          </Title>
+          <Text size={"lg"}  >{t("form.description")}</Text>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className={"flex flex-col gap-4 p-8"}>
+            <FormField
+              control={form.control}
+              name={"email"}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={"font-bold"}>{t("form.emailLabel")}</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name={"name"}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={"font-bold"}>{t("form.nameLabel")}</FormLabel>
+                  <FormControl>
+                    <Input  {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name={"title"}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={"font-bold"}>{t("form.titleLabel")}</FormLabel>
+                  <FormControl>
+                    <Input  {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name={"message"}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={"font-bold"}>{t("form.messageLabel")}</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              data-hover={CursorActionType.SEND}
+              data-hover-size={100}
+              size={'lg'}
+              className={"mt-4 w-full self-end bg-foreground !text-background"}
+              type={"submit"}>{t("form.sendMessageButton")}</Button>
+          </form>
+        </Form>
+      </Card>
+    </motion.div>
   );
 };
